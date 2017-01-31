3fb23b37d6c8dd9de0f4a0791df93ef214985cb6
Justin Roberts-Pierel <jr23@email.sc.edu>
2017-01-31 13:54:42 -0500

    started adding the objects in and describing them

---
+++ sntd/fits.py
@@ -0,0 +1,8 @@
+
+
+class fit:
+    '''
+    This is going to be a fits object that will contain a dictionary
+    of models with keys as model names. Each value will then be a model
+    class.
+    '''
