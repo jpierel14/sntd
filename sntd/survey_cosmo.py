48cfefaac98efcc776674a80f1ca3724946a0c78
Justin Roberts-Pierel <jr23@email.sc.edu>
2020-06-17 10:41:21 -0400

    trying to add H0 to survey cosmo

--- sntd/survey_cosmo.py
+++ sntd/survey_cosmo.py
@@ -643,7 +643,9 @@ class Survey(object):
 				vs = contour.get_paths()[0].vertices
 				x=vs[:,0]
 				y=vs[:,1]
-				area=0.5*np.sum(y[:-1]*np.diff(x) - x[:-1]*np.diff(y))
+				def PolyArea(x,y):
+    				return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+				#area=0.5*np.sum(y[:-1]*np.diff(x) - x[:-1]*np.diff(y))
 				area=np.abs(area)
 				line_name=self.name+': FOM=%.2f'%(np.pi/area)
 			else:
