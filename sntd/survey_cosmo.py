d50e3d20c0a0599e5b0cba9fbeb594ec164327e0
Justin Roberts-Pierel <jr23@email.sc.edu>
2020-06-18 08:43:33 -0400

    fixed some parallel t0 guessing for bad lcs

--- sntd/survey_cosmo.py
+++ sntd/survey_cosmo.py
@@ -908,8 +908,10 @@ class Fisher:
 		self._ii()
 
 		C = self.cov()
+		print(self.xvar,self.yvar)
 		C = C.take((self.ix,self.iy),0)
 		C = C.take((self.ix,self.iy),1)
+		print(C)
 		dx = np.sqrt(C[0,0])
 		dy = np.sqrt(C[1,1])
 		dxy = C[0,1]
